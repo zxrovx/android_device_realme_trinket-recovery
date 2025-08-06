8bb0d49cf70c1f14ef1e41880f5d409971031fd9
ZXROVX <91070944+zxrovx@users.noreply.github.com>
2025-08-06 23:04:19 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 
-DEVICE_PATH := device/realme/realme_trinket
+DEVICE_PATH := device/realme/trinket
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
@@ -173,5 +173,6 @@ TWRP_INCLUDE_LOGCAT := true
 
 # VINTF
 PRODUCT_ENFORCE_VINTF_MANIFEST := true
+
 
 
